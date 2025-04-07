2bd4425be13fa27d562b2684d2cc1b1b57dfc26c
Alkhemd <alankuk6@gmail.com>
2025-04-07 17:02:50 -0600

    first commit

---
+++ tarea2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+public class Factorial
+{
+
+    public static void Main()
+    { 
+      int numeros=5;
+      
+      int resultados= descomponer(numeros);
+       Console.WriteLine("el resultado es" + resultados);
+     
+     
+    }
+   
+     public static int descomponer(int numeros)
+      {
+          if(numeros <= 1)
+          {
+            return 1;   
+
+          }
+
+       else
+       {
+       return numeros * descomponer(numeros - 1);
+       }
+      
+      }
+
+    
+    
+    
+}
